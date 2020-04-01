f1a99cd7b2855df7648adbbce3c72e710d4228bb
Alexey <evil2142@bk.ru>
2020-04-01 17:06:24 +0300

    Was added Assert to tests.

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,3 +8,5 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), 'Login link is not presented'
