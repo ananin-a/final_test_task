c3fedec3ba852b02e7b5e8375a4e68123bfb01bc
Alexey <evil2142@bk.ru>
2020-04-09 09:11:51 +0300

    Minor changes, code optimization.

--- test_main_page.py
+++ test_main_page.py
@@ -11,7 +11,10 @@ from .pages.main_page import MainPage
 class TestLoginFromMainPage:
     def test_guest_should_see_login_link(self, browser):
         """Гостю видна ссылка прехода на страницу регистрации/авторизации."""
-        main_page = MainPage(browser, BasePageLocators.LINK_MAIN_PAGE)
+        main_page = MainPage(
+            browser,
+            BasePageLocators.LINK_MAIN_PAGE
+        )
         main_page.open()
         main_page.should_be_login_link()
 
@@ -20,16 +23,26 @@ class TestLoginFromMainPage:
         Пользователь гость может перейти на страницу регистрации/авторизации с главной страницы.
         Все поля регистрации/авторизации видны.
         """
-        main_page = MainPage(browser, BasePageLocators.LINK_MAIN_PAGE)
+        main_page = MainPage(
+            browser,
+            BasePageLocators.LINK_MAIN_PAGE
+        )
         main_page.open()
         main_page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
+
+        login_page = LoginPage(
+            browser,
+            browser.current_url
+        )
         login_page.should_be_login_page()
 
 
 def test_guest_login_page(browser):
     """Пользователь гость видит поля регистрации/аворизации."""
-    login_page = LoginPage(browser, LoginPageLocators.LINK_LOGIN_PAGE)
+    login_page = LoginPage(
+        browser,
+        LoginPageLocators.LINK_LOGIN_PAGE
+    )
     login_page.open()
     login_page.should_be_login_page()
 
@@ -39,9 +52,16 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     Переход под пользователем гость в пустую карзину. Проверка на то что корзина пуста,
     сообщение о том что корзина пуста присутствует.
     """
-    main_page = MainPage(browser, BasePageLocators.LINK_MAIN_PAGE)
+    main_page = MainPage(
+        browser,
+        BasePageLocators.LINK_MAIN_PAGE
+    )
     main_page.open()
     main_page.go_to_the_basket()
-    basket_page = BasketPage(browser, browser.current_url)
+
+    basket_page = BasketPage(
+        browser,
+        browser.current_url
+    )
     basket_page.should_not_be_proceed_to_checkout_button()
     basket_page.should_be_a_message_about_an_empty_basket()
--- test_product_page.py
+++ test_product_page.py
@@ -9,18 +9,23 @@ from .pages.product_page import ProductPage
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param(
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-                                      "/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.parametrize(
+    'link', [
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        pytest.param(
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+            marks=pytest.mark.xfail
+        ),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+    ]
+)
 def test_guest_can_add_product_to_basket(browser, link: str):
     """
     Пользователь гость, может добавить товар в корзину
@@ -39,7 +44,10 @@ def test_guest_can_add_product_to_basket(browser, link: str):
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     """Пользователь гость, не может видеть сообщение об успешном добавлении товара в корзину."""
-    product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+    product_page = ProductPage(
+        browser,
+        ProductPageLocators.LINK_PRODUCT_PAGE
+    )
     product_page.open()
     product_page.add_product_to_basket_no_alert()
     product_page.should_not_be_success_message()
@@ -47,7 +55,10 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
 def test_guest_cant_see_success_message(browser):
     """Пользователь гость, не видит сообщение о добавлении товара в корзину, не добавив товарю."""
-    product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+    product_page = ProductPage(
+        browser,
+        ProductPageLocators.LINK_PRODUCT_PAGE
+    )
     product_page.open()
     product_page.should_not_be_success_message()
 
@@ -55,7 +66,10 @@ def test_guest_cant_see_success_message(browser):
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     """Сообщение о добавлении товара в корзину исчезает."""
-    product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+    product_page = ProductPage(
+        browser,
+        ProductPageLocators.LINK_PRODUCT_PAGE
+    )
     product_page.open()
     product_page.add_product_to_basket_no_alert()
     product_page.should_element_disappearance()
@@ -63,7 +77,10 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 def test_guest_should_see_login_link_on_product_page(browser):
     """Пользоваьель гость, видит ссылку на страницу регистрации/авторизации со страницы товара."""
-    product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+    product_page = ProductPage(
+        browser,
+        ProductPageLocators.LINK_PRODUCT_PAGE
+    )
     product_page.open()
     product_page.should_be_login_link()
 
@@ -71,7 +88,10 @@ def test_guest_should_see_login_link_on_product_page(browser):
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     """Пользователь гость, может перейти на страницу регистрации/авторизации со страницы товара."""
-    product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+    product_page = ProductPage(
+        browser,
+        ProductPageLocators.LINK_PRODUCT_PAGE
+    )
     product_page.open()
     product_page.go_to_login_page()
 
@@ -79,16 +99,23 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     """Пользователь гость, не видит товар перейдя в корзину со страницы товара."""
-    product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+    product_page = ProductPage(
+        browser,
+        ProductPageLocators.LINK_PRODUCT_PAGE
+    )
     product_page.open()
     product_page.go_to_the_basket()
-    basket_page = BasketPage(browser, browser.current_url)
+
+    basket_page = BasketPage(
+        browser,
+        browser.current_url
+    )
     basket_page.should_not_be_proceed_to_checkout_button()
     basket_page.should_be_a_message_about_an_empty_basket()
 
 
 class TestUserAddToBasketFromProductPage:
-    """Зарегистрированный пользователь, может добавить товары в корзину."""
+    """Зарегистрированный пользователь, видит корзину и может добавить в нее товар."""
 
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -96,7 +123,10 @@ class TestUserAddToBasketFromProductPage:
         fake = Faker()
         email = fake.email()
         password = fake.password()
-        login_page = LoginPage(browser, LoginPageLocators.LINK_LOGIN_PAGE)
+        login_page = LoginPage(
+            browser,
+            LoginPageLocators.LINK_LOGIN_PAGE
+        )
         login_page.open()
         login_page.should_be_login_page()
         login_page.register_new_user(email, password)
@@ -104,7 +134,10 @@ class TestUserAddToBasketFromProductPage:
 
     def test_user_cant_see_success_message(self, browser):
         """Зарегистрированный пользователь, по умолчанию нет сообщения об успешном добавлении товара в корзину."""
-        product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+        product_page = ProductPage(
+            browser,
+            ProductPageLocators.LINK_PRODUCT_PAGE
+        )
         product_page.open()
         product_page.should_not_be_success_message()
 
@@ -115,7 +148,10 @@ class TestUserAddToBasketFromProductPage:
         Сравнение названия товара на странице и в корзине.
         Сравнение центы товара, цене товара в корзине.
         """
-        product_page = ProductPage(browser, ProductPageLocators.LINK_PRODUCT_PAGE)
+        product_page = ProductPage(
+            browser,
+            ProductPageLocators.LINK_PRODUCT_PAGE
+        )
         product_page.open()
         product_page.should_be_button_add_basket()
         product_page.should_not_be_success_message()
